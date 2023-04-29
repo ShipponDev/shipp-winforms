e70c19fb694e9c3aa5f0be2b4bf6a4ed8f72ebfc
ShipponDev <cassandra.mago@gmail.com>
2023-04-28 22:14:38 -0400

    Initial commit

---
+++ Consola/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Data;
+using CsvHelper;
+using System.Globalization;
+using Utilidades;
+
+namespace Consola
+{
+    public class Program
+    {
+        /// <summary>
+        /// MAIN
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("INICIO CONSOLA...");
+
+            try
+            {
+
+                string path = @"C:\Users\Cassandra Marchant\Desktop\PruebaConsola\2022 Carga EERR_CORP Magnus v1.0.csv";
+                string pathOutput = @"C:\Users\Cassandra Marchant\Desktop\PruebaConsola\2022 Carga EERR_CORP Magnus v1.0_" + DateTime.Today.ToString("yyyy_MM_dd") + ".xlsx";
+                char delimiter = ';';
+                DataTable dt = GeneraDt();
+
+                dt = ConvertCSVtoDataTable(path, delimiter);
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    Console.WriteLine(row["correlativo"]);
+
+                }
+
+                ConvertDataTableToExcel(dt, pathOutput);
+
+            }
+            catch
+            {
+                throw;
+            }
+
+            Console.WriteLine("FIN CONSOLA...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// LEE CSV
+        /// </summary>
+        /// <param name="strFilePath"></param>
+        /// <param name="delimiter"></param>
+        /// <returns></returns>
+        public static DataTable ConvertCSVtoDataTable(string strFilePath, char delimiter)
+        {
+
+            DataTable dt = new DataTable();
+
+            using (StreamReader sr = new StreamReader(strFilePath))
+            {
+
+                string[] headers = sr.ReadLine().Split(delimiter);
+
+                foreach (string header in headers)
+                {
+
+                    dt.Columns.Add(header);
+
+                }
+
+                while (!sr.EndOfStream)
+                {
+
+                    string[] rows = sr.ReadLine().Split(delimiter);
+                    DataRow dr = dt.NewRow();
+
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+
+                        dr[i] = rows[i];
+
+                    }
+
+                    dt.Rows.Add(dr);
+                }
+
+            }
+
+
+            return dt;
+        }
+
+        /// <summary>
+        /// GENERA REPORTE EXCEL
+        /// </summary>
+        /// <param name="dtReporte"></param>
+        public static void ConvertDataTableToExcel(DataTable dt, string pathFile)
+        {
+
+            //============================================================
+            // DECLARA VARIABLES
+            //============================================================
+            string nameSheet = "Data";
+
+            try
+            {
+
+                Console.WriteLine("INICIO EXCEL");
+
+                //============================================================
+                // GENERA EXCEL
+                //============================================================
+                UExcel Excel = new UExcel();
+                Excel.GeneraExcelReporte(dt, pathFile, nameSheet, "");
+
+                Console.WriteLine("FIN EXCEL");
+
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// CABECERA CSV
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable GeneraDt()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add(new DataColumn("correlativo", typeof(string)));
+            dt.Columns.Add(new DataColumn("cod_uni_contable", typeof(string)));
+            dt.Columns.Add(new DataColumn("cod_dpto", typeof(string)));
+            dt.Columns.Add(new DataColumn("cod_cta", typeof(string)));
+            dt.Columns.Add(new DataColumn("nom_cta", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_1", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_2", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_3", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_4", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_5", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_6", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_7", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_8", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_9", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_10", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_11", typeof(string)));
+            dt.Columns.Add(new DataColumn("mes_12", typeof(string)));
+            dt.Columns.Add(new DataColumn("total", typeof(string)));
+
+            return dt;
+        }
+    }
+}
---
+++ Consola/UExcel.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+
+namespace Utilidades
+{
+    public class UExcel
+    {
+
+        public bool GeneraExcelReporte(DataTable dataTable, string rutaExcel, string nombreHoja, string ReporType)
+        {
+
+            //============================================================
+            // DECLARA VARIABLES GLOBALES
+            //============================================================
+            //bool retorno = false;
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+            Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+            Microsoft.Office.Interop.Excel.Range excelCellrange;
+
+            try
+            {
+                //============================================================
+                // INICIA EXCEL Y GENERA OBJETO
+                //============================================================
+                excel = new Microsoft.Office.Interop.Excel.Application();
+
+
+                //============================================================
+                // HACE VISIBLE EXCEL
+                //============================================================
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+
+                //============================================================
+                // GENERA LIBRO DE TRABAJO
+                //============================================================
+                excelworkBook = excel.Workbooks.Add(Type.Missing);
+
+                //============================================================
+                // GENERA HOJA DE TRABAJO
+                //============================================================
+                excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.ActiveSheet;
+                excelSheet.Name = nombreHoja;
+
+
+                //============================================================
+                // RECORRE FILAS Y AGREGA VALORES A HOJA
+                //============================================================
+                int rowcount = 1;
+
+                if (dataTable.Rows.Count > 0)
+                {
+
+                    //for (int x=0;x<dataTable.Columns.Count)
+                    //excelSheet.Cells[0, 1] = dataTable.Columns[3 - 1].ColumnName;
+
+
+                    /*
+
+                    excelSheet.Cells[0, 1] = dataTable.Columns[3 - 1].ColumnName;
+                    excelSheet.Cells.Font.Color = System.Drawing.Color.Black;
+                    */
+                }
+
+
+                foreach (DataRow datarow in dataTable.Rows)
+                {
+
+                    rowcount += 1;
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        // on the first iteration we add the column headers
+                        if (rowcount == 2)
+                        {
+
+
+                            excelSheet.Cells[1, i] = dataTable.Columns[i - 1].ColumnName;
+                            excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+
+                            if (i == dataTable.Columns.Count)
+                            {
+                                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[1, dataTable.Columns.Count]];
+                                FormattingExcelCells(excelCellrange, "#305496", System.Drawing.Color.White, true);
+                            }
+
+
+
+
+
+                        }
+
+
+
+
+                        //for alternate rows
+                        if (rowcount > 2)
+                        {
+                            excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+
+
+                            if (i == dataTable.Columns.Count)
+                            {
+                                if (rowcount % 2 == 0)
+                                {
+
+                                    excelCellrange = excelSheet.Range[excelSheet.Cells[rowcount, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                                    //FormattingExcelCells(excelCellrange, "#FFFFFF", System.Drawing.Color.Black, false);
+
+                                }
+                                else
+                                {
+
+                                    excelCellrange = excelSheet.Range[excelSheet.Cells[rowcount, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                                    //FormattingExcelCells(excelCellrange, "#B4C6E7", System.Drawing.Color.Black, false);
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+
+                    /*
+
+                    rowcount += 1;
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        // on the first iteration we add the column headers
+                        if (rowcount == 2)
+                        {
+                            Console.WriteLine(dataTable.Columns[i - 1].ColumnName);
+                            excelSheet.Cells[1, i] = dataTable.Columns[i - 1].ColumnName;
+                            excelSheet.Cells.Font.FontStyle = "Negrita";// [1, i]
+                            excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                            FormattingExcelCells(excelCellrange, "#305496", System.Drawing.Color.White, false);
+                            
+
+                        }
+
+                        
+
+                        //for alternate rows
+                        if (rowcount > 2)
+                        {
+                            excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+                            excelSheet.Cells.Font.FontStyle = "Normal";// [1, i]
+
+                            if (i == dataTable.Columns.Count)
+                            {
+                                if (rowcount % 2 == 0)
+                                {
+                                    excelCellrange = excelSheet.Range[excelSheet.Cells[rowcount, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                                    FormattingExcelCells(excelCellrange, "#B4C6E7", System.Drawing.Color.Black, false);
+                                }
+
+                            }
+                        }
+
+                    }
+                    */
+
+                }
+
+
+                //============================================================
+                // RECORRE FILAS Y AGREGA VALORES A HOJA
+                //============================================================
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                excelCellrange.EntireColumn.AutoFit();
+                Microsoft.Office.Interop.Excel.Borders border = excelCellrange.Borders;
+                border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                border.Weight = 2d;
+
+
+
+
+                excelworkBook.SaveAs(rutaExcel); ;
+                excelworkBook.Close();
+                excel.Quit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                excelSheet = null;
+                excelCellrange = null;
+                excelworkBook = null;
+            }
+
+        }
+
+
+        public static void FormattingExcelCells(Microsoft.Office.Interop.Excel.Range range, string HTMLcolorCode, System.Drawing.Color fontColor, bool IsFontbool)
+        {
+            range.Interior.Color = System.Drawing.ColorTranslator.FromHtml(HTMLcolorCode);
+            range.Font.Color = System.Drawing.ColorTranslator.ToOle(fontColor);
+            if (IsFontbool == true)
+            {
+                range.Font.Bold = IsFontbool;
+            }
+        }
+
+    }
+}
